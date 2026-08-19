687acfb7658649424dc8177133c4b0f2b4393039
azraji <zdine30@gmail.com>
2026-08-19 19:34:11 +0100

    fix requestbody error

--- product-service/src/main/java/com/microservices/product_service/controller/productController.java
+++ product-service/src/main/java/com/microservices/product_service/controller/productController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class productController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    private void AddProduct(productRequest productRspons) {
+    private void AddProduct(@RequestBody productRequest productRspons) {
         productService.AddProduct(productRspons);
     }
 
--- product-service/src/main/java/com/microservices/product_service/model/Product.java
+++ product-service/src/main/java/com/microservices/product_service/model/Product.java
@@ -3,13 +3,17 @@ package com.microservices.product_service.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Document(value="product")
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     @Id
     private String id;

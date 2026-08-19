75940d781b675077821ee146b45033c5dd8891f8
azraji <zdine30@gmail.com>
2026-08-19 19:25:43 +0100

    get all product

--- product-service/src/main/java/com/microservices/product_service/repository/productRepository.java
+++ product-service/src/main/java/com/microservices/product_service/repository/productRepository.java
@@ -1,5 +1,18 @@
 package com.microservices.product_service.repository;
 
-public class productRepository {
-    
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.microservices.product_service.model.Product;
+
+/*
+save(product);
+findAll();
+findById(id);
+deleteById(id);
+delete(product);
+existsById(id);
+count();
+*/
+public interface productRepository extends MongoRepository<Product, String> {
+
 }

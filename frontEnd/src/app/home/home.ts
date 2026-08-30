8c7a2d4a09ad0c50fde311ad1ee4113100142850
RedaAz07 <annizreda07@gmail.com>
2026-08-30 14:56:56 +0100

    Refactor navbar and layout structure; remove unused components and styles
    - Deleted navbar component and its associated styles and HTML.
    - Introduced a new main layout component to encapsulate the navbar and router outlet.
    - Updated product and home components to reflect the new layout structure.
    - Refactored role guard to include JWT validation and role checking.
    - Cleaned up product and post components for better readability and performance.
    - Added a new dashboard component with basic structure and test cases.
    - Consolidated styles for navbar into a new layout-specific CSS file.

--- frontEnd/src/app/home/home.ts
+++ frontEnd/src/app/home/home.ts
@@ -1,12 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
-import { Navbar } from '../components/navbar/navbar';
 import { HttpClient } from '@angular/common/http';
 import { Productdto } from '../core/models/post';
 import { Post } from '../components/post/post';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Post],
+  imports: [ Post],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
